4c80eb8b3b8d7d6cd4d15fcd9a6f16964717011f
40323130 <40323130@gm.nfu.edu.tw>
2016-05-05 18:02:53 +0800

    task1 correct

--- users/a/g4/ag4_40323130.py
+++ users/a/g4/ag4_40323130.py
@@ -112,6 +112,142 @@ def O(x, y, rx, ry, rot, color, border, linewidth):
     basic19.rotate(90)
     basic19.translate(20, 0)
     
+    basic110 = cmbr.dup()
+    basic110.rotate(0)
+    basic110.translate(0, 0)
+    
+    basic20 = cmbr.dup()
+    basic20.rotate(0)
+    basic20.translate(60, 20)
+    
+    basic21 = cmbr.dup()
+    basic21.rotate(170)
+    basic21.translate(60, 20)
+    
+    basic22 = cmbr.dup()
+    basic22.rotate(165)
+    basic22.translate(64, 40)
+    
+    basic23 = cmbr.dup()
+    basic23.rotate(90)
+    basic23.translate(69.5, 60)
+    
+    basic24 = cmbr.dup()
+    basic24.rotate(197)
+    basic24.translate(96, 40)
+    
+    basic25 = cmbr.dup()
+    basic25.rotate(192)
+    basic25.translate(100, 20)
+    
+    basic26 = cmbr.dup()
+    basic26.rotate(0)
+    basic26.translate(100, 20)
+    
+    basic27 = cmbr.dup()
+    basic27.rotate(0)
+    basic27.translate(100, 0)
+    
+    basic28 = cmbr.dup()
+    basic28.rotate(90)
+    basic28.translate(60, 0)
+    
+    basic29 = cmbr.dup()
+    basic29.rotate(90)
+    basic29.translate(80, 0)
+    
+    basic210 = cmbr.dup()
+    basic210.rotate(0)
+    basic210.translate(60, 0)
+    
+    basic30 = cmbr.dup()
+    basic30.rotate(0)
+    basic30.translate(120, 20)
+    
+    basic31 = cmbr.dup()
+    basic31.rotate(170)
+    basic31.translate(120, 20)
+    
+    basic32 = cmbr.dup()
+    basic32.rotate(165)
+    basic32.translate(124, 40)
+    
+    basic33 = cmbr.dup()
+    basic33.rotate(90)
+    basic33.translate(129.5, 60)
+    
+    basic34 = cmbr.dup()
+    basic34.rotate(197)
+    basic34.translate(156, 40)
+    
+    basic35 = cmbr.dup()
+    basic35.rotate(192)
+    basic35.translate(160, 20)
+    
+    basic36 = cmbr.dup()
+    basic36.rotate(0)
+    basic36.translate(160, 20)
+    
+    basic37 = cmbr.dup()
+    basic37.rotate(0)
+    basic37.translate(160, 0)
+    
+    basic38 = cmbr.dup()
+    basic38.rotate(90)
+    basic38.translate(120, 0)
+    
+    basic39 = cmbr.dup()
+    basic39.rotate(90)
+    basic39.translate(140, 0)
+    
+    basic310 = cmbr.dup()
+    basic310.rotate(0)
+    basic310.translate(120, 0)
+    
+    basic40 = cmbr.dup()
+    basic40.rotate(0)
+    basic40.translate(180, 20)
+    
+    basic41 = cmbr.dup()
+    basic41.rotate(170)
+    basic41.translate(180, 20)
+    
+    basic42 = cmbr.dup()
+    basic42.rotate(165)
+    basic42.translate(184, 40)
+    
+    basic43 = cmbr.dup()
+    basic43.rotate(90)
+    basic43.translate(189.5, 60)
+    
+    basic44 = cmbr.dup()
+    basic44.rotate(197)
+    basic44.translate(216, 40)
+    
+    basic45 = cmbr.dup()
+    basic45.rotate(192)
+    basic45.translate(220, 20)
+    
+    basic46 = cmbr.dup()
+    basic46.rotate(0)
+    basic46.translate(220, 20)
+    
+    basic47 = cmbr.dup()
+    basic47.rotate(0)
+    basic47.translate(220, 0)
+    
+    basic48 = cmbr.dup()
+    basic48.rotate(90)
+    basic48.translate(180, 0)
+    
+    basic49 = cmbr.dup()
+    basic49.rotate(90)
+    basic49.translate(200, 0)
+    
+    basic410 = cmbr.dup()
+    basic410.rotate(0)
+    basic410.translate(180, 0)
+    
     cmbr.appendPath(basic10)
     cmbr.appendPath(basic11)
     cmbr.appendPath(basic12)
@@ -122,6 +258,40 @@ def O(x, y, rx, ry, rot, color, border, linewidth):
     cmbr.appendPath(basic17)
     cmbr.appendPath(basic18)
     cmbr.appendPath(basic19)
+    cmbr.appendPath(basic110)
+    cmbr.appendPath(basic20)
+    cmbr.appendPath(basic21)
+    cmbr.appendPath(basic22)
+    cmbr.appendPath(basic23)
+    cmbr.appendPath(basic24)
+    cmbr.appendPath(basic25)
+    cmbr.appendPath(basic26)
+    cmbr.appendPath(basic27)
+    cmbr.appendPath(basic28)
+    cmbr.appendPath(basic29)
+    cmbr.appendPath(basic210)
+    cmbr.appendPath(basic30)
+    cmbr.appendPath(basic31)
+    cmbr.appendPath(basic32)
+    cmbr.appendPath(basic33)
+    cmbr.appendPath(basic34)
+    cmbr.appendPath(basic35)
+    cmbr.appendPath(basic36)
+    cmbr.appendPath(basic37)
+    cmbr.appendPath(basic38)
+    cmbr.appendPath(basic39)
+    cmbr.appendPath(basic310)
+    cmbr.appendPath(basic40)
+    cmbr.appendPath(basic41)
+    cmbr.appendPath(basic42)
+    cmbr.appendPath(basic43)
+    cmbr.appendPath(basic44)
+    cmbr.appendPath(basic45)
+    cmbr.appendPath(basic46)
+    cmbr.appendPath(basic47)
+    cmbr.appendPath(basic48)
+    cmbr.appendPath(basic49)
+    cmbr.appendPath(basic410)
 
     # hole 為原點位置
     hole = cobj(shapedefs.circle(4), "PATH")
